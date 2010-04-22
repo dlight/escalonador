f4a11c4c3c2128a5097893c92923052692851e13
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2010-04-22 00:42:57 -0300

    Aparentemente, FIFO sem SO feito

--- gen.py
+++ gen.py
@@ -1,43 +1,86 @@
+from heapq import *
+
 class Processo:
+    # ps: acoes de executar e dormir estao sempre alternadas
+    # a primeira acao e' sempre uma execucao
+
     def __init__(self, nome, acoes):
         self.nome = nome
         self.acoes = acoes
-    def nova_acao(self, tipo, tempo):
-        self.acoes.append((tipo, tempo))
+    def ver_acao(self):
+        self.acoes[0]
     def proxima_acao(self):
-        self.acoes.pop(0)
+        return self.acoes.pop(0)
 
-def proximo(
+class Fila_de_espera:
+    fila = []
+
+    def dormir(self, tempo, nome):
+        heappush(self.fila, (tempo, nome))
+    def acordar(self):
+        tempo, acordado = heappop(self.fila)
+        return acordado
+    def tempo_a_acordar(self):
+        tempo, nome = heappop(self.fila)
+        self.dormir(tempo, nome)
+        return tempo
+    def alguem_vai_acordar(self, em_certo_tempo):
+        return self.fila and self.tempo_a_acordar() < em_certo_tempo
+    def despertador(self, em_certo_tempo):
+        while self.alguem_vai_acordar(em_certo_tempo):
+           yield self.acordar()
+    def tick(self, tempo_a_subtrair):
+        self.fila = [(tempo - tempo_a_subtrair, nome) for (tempo, nome) in self.fila]
+
 
 def escalonador_fifo(prontos):
-    espera = []
-    while prontos:
-        
+    espera = Fila_de_espera()
+    while prontos or espera.fila:
+        if not prontos:
+            tempo_ocioso = espera.tempo_a_acordar()
+            pronto = espera.acordar()
+            prontos.append(pronto)            
+            espera.tick(tempo_ocioso)
+            continue
+
         proximo = prontos.pop(0)
-        tipo, tempo = proximo.proxima_acao()
-        yield [proximo.nome, tipo, tempo]
-        if proximo.acoes == []:
-            yield [proximo.nome, 'fim']
+        tempo_executando = proximo.proxima_acao()
+
+        yield ['exec', proximo.nome, tempo_executando]
+
+        for pronto in espera.despertador(tempo_executando):
+            prontos.append(pronto)
+            yield ['acordou', pronto.nome]
+
+        espera.tick(tempo_executando)
+
+        if not proximo.acoes:
+            yield ['terminou', proximo.nome]
         else:
-            prontos.append(proximo)
+            tempo_a_dormir = proximo.proxima_acao()
+            espera.dormir(tempo_a_dormir, proximo)
+            yield ['dormiu', proximo.nome]
 
 def passo(acao):
-    nome = acao.pop(0)
-    tipo = acao.pop(0)
-    if tipo == 'fim':
-        #print 'amm'
-        print "Processo finalizado: %s" % nome
-        #print 'err'
+    tipo, nome = acao[:2]
+    if tipo == 'terminou':
+        print "%s terminou." % nome
+    elif tipo == 'acordou':
+        print "%s acordou." % nome
+    elif tipo == 'dormiu':
+        print "%s dormiu." % nome
+    elif tipo == 'exec':
+        tempo = acao[2]
+        print "%s executa por %ss." %(nome, tempo)
     else:
-        tempo = acao.pop(0)
-        print "Processo %s fica %s por %s segundos" % (nome, tipo, tempo)
+        raise Exception("Acao desconhecida?")
 
 def executar(escalonador, processos):
     for acao in escalonador(processos):
         passo(acao)
 
 
-a = Processo('a', [('ahm', 1), ('wait', 10)])
-b = Processo('b', [('ahm', 20), ('wait', 1)])
+a = Processo('a', [1, 10, 3])
+b = Processo('b', [20, 1, 5, 2, 3, 5, 6])
 
 def e(): executar(escalonador_fifo, [a, b])
